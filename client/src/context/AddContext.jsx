13d78cd78bbfc901ff47d2b2be8c75a9caa150bb
Berita-Winnie <beritawinnie@gmail.com>
2026-02-08 16:38:50 +0300

    Added course card in course section

--- client/src/context/AddContext.jsx
+++ client/src/context/AddContext.jsx
@@ -1,9 +1,25 @@
-import { createContext } from 'react'
-
+import { createContext, useState } from 'react'
+import { dummyCourses } from '../assets/assets'
+import { useEffect } from 'react'
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
-  const value = {}
+  const currency = import.meta.env.VITE_CURRENCY
+  const [allCourses, setAllCourses] = useState([])
+  //Fetch All Courses
+
+  const fetchAllCourses = async () => {
+    setAllCourses(dummyCourses)
+  }
+
+  useEffect(() => {
+    fetchAllCourses()
+  }, [])
+
+  const value = {
+    currency,
+    allCourses,
+  }
   return (
     <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
   )
